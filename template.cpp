7a1b4033ff64b15c926d5533774f3c5e35349da2
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-08-24 16:22:27 +0800

    up

---
+++ template.cpp
@@ -0,0 +1,15 @@
+// window1.cpp
+#include <GL/glut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);  // Clear screen
+    glFlush();  // Render now
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);                    // Initialize GLUT
+    glutCreateWindow("My first Open GL Program ");    // Create window
+    glutDisplayFunc(display);                 // Set display callback
+    glutMainLoop();                           // Enter event loop
+    return 0;
+}
