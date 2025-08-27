244bae71e1a6157033b0355de346d6468f6d3d5e
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-08-27 16:08:24 +0800

    Create sample6.cpp

---
+++ Module1/cpp/sample6.cpp
@@ -0,0 +1,23 @@
+//Program 6: Multi-dimensional Arrays
+//This program demonstrates the use of multi-dimensional arrays.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    cout << "Matrix elements:" << endl;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
