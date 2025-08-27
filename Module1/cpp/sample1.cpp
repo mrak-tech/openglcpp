ef1dad1d9173e25ea37591369fb942adcd8618c1
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-08-27 15:55:01 +0800

    Create sample1.cpp

---
+++ Module1/cpp/sample1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 42;       // Declare an integer variable
+    int* ptr = &x;    // Declare a pointer and assign it the address of x
+
+    cout << "Value of x: " << x << endl;
+    cout << "Address of x: " << &x << endl;
+    cout << "Value stored in ptr (address of x): " << ptr << endl;
+    cout << "Value pointed to by ptr: " << *ptr << endl;
+
+    *ptr = 100;       // Modify the value of x through the pointer
+    cout << "New value of x: " << x << endl;
+
+    return 0;
+}
