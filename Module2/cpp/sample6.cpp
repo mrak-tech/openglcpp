5c20d3b55684b749bd28bf18855d53f8be665511
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-14 20:05:57 +0800

    Create sample6.cpp

---
+++ Module2/cpp/sample6.cpp
@@ -0,0 +1,16 @@
+//No Parameter, No Return Value
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void sayHello();
+
+int main() {
+    sayHello(); // Call the function
+    return 0;
+}
+
+// Function definition
+void sayHello() {
+    cout << "Hello, world!" << endl;
+}
