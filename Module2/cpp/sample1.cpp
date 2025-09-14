6a0e1e0aee66b6e723cea7845373276ffb768f85
Hadji Tejuco <hadji.tejuco@gmail.com>
2025-09-14 20:02:41 +0800

    Create sample1.cpp

---
+++ Module2/cpp/sample1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void displayMessage();
+
+int main() {
+    displayMessage(); // Call the function
+    return 0;
+}
+
+// Function definition
+void displayMessage() {
+    cout << "Hello, this is a simple function!" << endl;
+}
